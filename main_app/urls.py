fc9b834571bb00fb42e126ce0821644623656101
jestricklandn <jeliotstrickland@gmail.com>
2017-12-17 09:10:37 -0700

    Added detail page with back link.

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+#urlpatterns = [
+    #url(r'^$', views.index)
+#]
+
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name = 'index'),
+    url(r'^([0-9]+)/$',
+        views.detail, name = 'detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,10 @@ def index(request):
     budget_items = Budget_Item.objects.all()
     return render(request, 'index.html', {'budget_items':budget_items})
 
+def detail(request, Budget_Item_id):
+    budget_item = Budget_Item.objects.get(id=Budget_Item_id)
+    return render(request, 'detail.html', {'budget_item':budget_item})
+
 #class Budget_Item:
     #def __init__(self, name, amount, date, description):
         #self.name = name
